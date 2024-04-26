79289af85badce61cc8c540f4720c8fb2d8a24c0
Tomek <29851015+bxtom@users.noreply.github.com>
2024-04-26 18:04:41 +0200

    favMovies

---
+++ data.py
@@ -0,0 +1,31 @@
+favMovies = [
+    {
+        "tytuł": "Event Horizon", 
+        "rok": 1997,
+        "ocena": 6.6,
+        "opis": "A rescue crew is tasked with investigating the mysterious reappearance of a spaceship that had been lost for seven years.",
+    },
+    {
+        "tytuł": "2001: A Space Odyssey", 
+        "rok": 1968,
+        "ocena": 8.3,
+        "opis": "After uncovering a mysterious artifact buried beneath the Lunar surface, a spacecraft is sent to Jupiter to find its origins: a spacecraft manned by two men and the supercomputer HAL 9000.",
+    },
+    {
+        "tytuł": "First Blood", 
+        "rok": 1982,
+        "ocena": 7.7,
+        "opis": "A veteran Green Beret is forced by a cruel Sheriff and his deputies to flee into the mountains and wage an escalating one-man war against his pursuers.",
+    },
+    {
+        "tytuł": "Terminator 2: Judgment Day", 
+        "rok": 1991,
+        "ocena": 8.6,
+        "opis": "A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg.",
+    },
+]
+
+print(favMovies[0]["tytuł"])
+print(favMovies[1]["rok"])
+print(favMovies[2]["ocena"])
+print(favMovies[3]["opis"])
